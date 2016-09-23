fca6c55fc9724783a5ce0d6e9986f99026f4eaed
parthmishra <pmishraworld@mac.com>
2016-09-23 14:00:03 -0600

    week 5

---
+++ week5.m
@@ -0,0 +1,27 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf470
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 T/F The VC-dimension is the maximum number of points that can be shattered in an infinite number of ways. Explain.\
+\
+False, the VC-dimension refers to the maximum number of points that can be shattered *at all*. Not all sets of points can be shattered in an infinite number of ways i.e. there may be only one way to shatter some number of points, but that doesn\'92t change it\'92s dimensionality.\
+\
+T/F SVMs, Naive Bayes, and logistic regression all find a hyperplane to separate data. Explain.\
+\
+True, If we take hyperplane to be equivalent to \'93decision boundary/surface\'94 then all 3 satisfy this definition. All of them are linear classifiers that essentially form a hyperplane decision boundary via varying metrics.\
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+\cf0 \
+Which has higher entropy? A rare word or a common word? Explain.\
+\
+A rare word is more likely to have higher entropy due to the fact that more information is gained from a new, rare word compared to an common word. The frequency of a common word dilutes its information gain compared to the scarcity of a rare word, thus a rare word gives more information gain.\
+\
+What is a Rademacher variable, and what function does it serve in terms of determining Rademacher complexity?\
+\
+Rademacher variable is a random variable that provides symmetry to a correlation function in determining Rademacher complexity.\
+\
+\
+\
+\
+}
